1b2ee325bd65df2661f0c169509b50f2ef851dd8
Alex <alexandriyanov97@gmail.com>
2019-05-13 17:16:39 +0700

    add event and MailSendler

---
+++ EventGenerator/Models/Repository/ActivityFinishedRepository.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+using Models.Activity.Repository;
+using MongoDB.Driver;
+
+namespace EventGenerator.Repository
+{
+    public class ActivityFinishedRepository
+    {
+        private readonly IMongoCollection<ActivityFinishedInfo> activityFinishedInfo;
+
+        public ActivityFinishedRepository(Configuration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("SkimaDb"));
+            var database = client.GetDatabase("SkimaDb");
+            activityFinishedInfo = database.GetCollection<ActivityFinishedInfo>("ActivityFinishedInfo");
+        }
+        
+        
+        public Task<List<ActivityFinishedInfo>> GetAllActivityEventInfo()
+        {
+            var search = activityFinishedInfo.Find(info => true);
+            var result = search.ToList();
+            return Task.FromResult(result);
+        }
+        
+        public Task<ActivityFinishedInfo> CreateActivityFinishedInfoAsync(ActivityFinishedInfo newActivityFinishedInfo)
+        {
+            if (newActivityFinishedInfo == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            activityFinishedInfo.InsertOne(newActivityFinishedInfo);
+            return Task.FromResult(newActivityFinishedInfo);
+        }
+        
+        public Task UpdateAsync(string id, ActivityFinishedInfo activityFinishedInfo)
+        {
+            this.activityFinishedInfo.ReplaceOne(user => user.Id == id, activityFinishedInfo);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ EventGenerator/Settings/Repository/SettingsRepository.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Threading.Tasks;
+using Models;
+using MongoDB.Driver;
+
+namespace EventGenerator.Repository
+{
+    public class SettingsRepository
+    {
+        private readonly IMongoCollection<SettingsEventOfRegistration> settingsEventOfRegistration;
+
+        private readonly IMongoCollection<SettingsEventOfSubscribeToActivity> settigsEventOfSubscribeToActivity;
+
+        private readonly IMongoCollection<SettingsEventOfActivity> settingsEventOfActivity;
+
+        private readonly IMongoCollection<SettingsEventOfStartSprint> settingsEventOfStartSprint;
+
+
+        public SettingsRepository(Configuration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("SkimaDb"));
+            var database = client.GetDatabase("SkimaDb");
+            settingsEventOfRegistration = database.GetCollection<SettingsEventOfRegistration>("SettingsEventOfRegistration");
+            settigsEventOfSubscribeToActivity =
+                database.GetCollection<SettingsEventOfSubscribeToActivity>("SettigsEventOfSubscribeToActivity");
+            settingsEventOfActivity = database.GetCollection<SettingsEventOfActivity>("SettingsEventOfActivity");
+            settingsEventOfStartSprint = database.GetCollection<SettingsEventOfStartSprint>("SettingsEventOfStartSprint");
+        }
+
+        public Task<SettingsEventOfRegistration> GetLastRegistrationSettings()
+        {
+            var lastUpdate = settingsEventOfRegistration.Find(setting => true).SortByDescending(x => x.DateOfLastNotificationUser).First();
+            return Task.FromResult(lastUpdate);
+        }
+
+        public Task<SettingsEventOfRegistration> CreateRegistrationEventSettingsAsync(SettingsEventOfRegistration settingEvent)
+        {
+            if (settingEvent == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            settingsEventOfRegistration.InsertOne(settingEvent);
+            return Task.FromResult(settingEvent);
+        }
+
+        public Task<SettingsEventOfSubscribeToActivity> GetLastSubscribeSettings()
+        {
+            var lastUpdate = settigsEventOfSubscribeToActivity.Find(settings => true).SortByDescending(x => x.CreatedAt)
+                .First();
+            return Task.FromResult(lastUpdate);
+        }
+
+        public Task<SettingsEventOfSubscribeToActivity> CreateSubscribeToActivitySettings(SettingsEventOfSubscribeToActivity settingEvent)
+        {
+            if (settingEvent == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            settigsEventOfSubscribeToActivity.InsertOne(settingEvent);
+            return Task.FromResult<SettingsEventOfSubscribeToActivity>(settingEvent);
+        }
+
+        public Task<SettingsEventOfActivity> GetLastActivitySettings()
+        {
+            var lastUpdate = settingsEventOfActivity.Find(setting => true)
+                .SortByDescending(x => x.DateOfLastCheckedActivity).First();
+            return Task.FromResult(lastUpdate);
+        }
+
+        public Task<SettingsEventOfActivity> CreateLastActivitySettings(SettingsEventOfActivity setting)
+        {
+            if (setting == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            settingsEventOfActivity.InsertOne(setting);
+            return Task.FromResult(setting);
+        }
+
+        public Task<SettingsEventOfStartSprint> GetLastStartSprintSettings()
+        {
+            var lastUpdate = settingsEventOfStartSprint.Find(setting => true).SortByDescending(x=>x.DateOfLastCheckedSprint).First();
+            return Task.FromResult(lastUpdate);
+        }
+
+        public Task<SettingsEventOfStartSprint> CreateLastStartSprintSettings(SettingsEventOfStartSprint setting)
+        {
+            if (setting == null)
+            {
+                throw new ArgumentNullException();
+            }
+            
+            settingsEventOfStartSprint.InsertOne(setting);
+            return Task.FromResult(setting);
+        }
+
+
+    }
+}
